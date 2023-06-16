a7350f5827e851ad36ae11868025422b3718a6bb
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-16 10:17:43 -0700

    C program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry pioint to function
+ * Return: returns 0 and exits the function
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
