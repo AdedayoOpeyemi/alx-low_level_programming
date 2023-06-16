45bab93f1ef63172375bf93972a39380f6856824
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-16 06:20:01 -0700

    program will assign a random number to the variable n and print positive, negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - function to print positive, negative or zero depending on value of variable n
+ * Return: return 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if( n < 0 )
+	{
+		printf("%d\n  is negative", n );
+	} else if( n > 0)
+	{
+		printf(" %d\n  is positive", n );
+	} else
+	{
+		printf(" %d\n  is zero", n );
+	}
+
+	return (0);
+}
