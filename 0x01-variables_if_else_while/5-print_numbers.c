48841a28fb060f63568eaba55cee647fddb71268
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-16 09:49:25 -0700

    C program to print from 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry pioint to function
+ * Return: returns 0 and exits the function
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
