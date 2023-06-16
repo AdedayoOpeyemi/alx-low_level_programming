b99a1ea42873d08de8705ebc730696269f3514ed
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-16 08:20:08 -0700

    C program to print out all lower cade of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry pioint to function
+ * Return: returns 0 and exits the function
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return(0)
+}
