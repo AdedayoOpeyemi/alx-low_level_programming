0ded7f7b13e5954a313d1dfe5c72245f129f2db1
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-16 09:11:00 -0700

    C program to print lower and upper case alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,22 @@
 
 int main(void)
 {
-        char letter = 'a';
+	char letter = 'a';
 	char upper = 'A';
-
-        while (letter <= 'z')
-        {
-                putchar(letter);
-                letter++;
-        }
-
+	
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	
 	while (upper <= 'Z')
 	{
 		putchar(upper);
 		upper++;
 	}
 
-        putchar('\n');
-
-        return (0);
+	putchar('\n');
+	
+	return (0);
 }
