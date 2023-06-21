9d8f818ad09fadab0c73ad3a53db09d3edff2c6d
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-20 19:12:35 -0700

    C program to check if a character is lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * function_name - Short description, single line
+ * Return: Description of the returned value
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
