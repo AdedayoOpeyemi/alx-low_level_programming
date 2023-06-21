105b2e91d32a3f52c91b7b37bad745b919a82417
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-20 19:45:24 -0700

    C program that checks whether an argument is positive, negative or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign -program to check if an argument is positive, negative or zero
+ * @n: argument to be checked
+ * Return: 1 if argument is lower case and 0 if not
+ */
+
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else if (c < 0)
+	{
+		_puthcar('-');
+		return (-1);
+	}
+}
