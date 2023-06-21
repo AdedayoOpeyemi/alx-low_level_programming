962ad846172c8989f87a87a759e07bfe480c61b9
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-20 21:36:30 -0700

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,32 @@
 #include "main.h"
 
 /**
- * _islower -program to check if an argument is lowercase
+ * print_to_98 -program to check if an argument is lowercase
  * @n: argument to be checked
  * Return: void
  */
 
 void print_to_98(int n)
 {
-
-	int i;
+	int i, j;
 
 	if (n <= 98)
 	{
-		for (i = n; i < 98; i++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%i, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-	} else
+	} else if (n >= 98)
 	{
-		for (i = n; i > 98; i--)
+		for (j = n; j >= 98; j--)
 		{
-			printf("%i, ", i);
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	printf("98\n");
 }
