267fb1942218bf429f2cdafd5d6383267879188f
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-20 19:34:11 -0700

    updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _islower -program to check if an argument is uppercase
+ * _isalpha -program to check if an argument is uppercase or lowercase
  * @c: argument to be checked
  * Return: 1 if argument is upper case and 0 if not
  */
 
 int _isalpha(int c)
 {
-        return ((c >= 'A' && c <= 'Z') || (c>= 'a' && c <= 'z'));
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
