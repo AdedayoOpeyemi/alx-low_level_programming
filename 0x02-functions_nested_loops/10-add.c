f97698347080fc8b5777102b156ba131f98a846f
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-20 21:14:07 -0700

    program to sum 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add -program to check if an argument is lowercase
+ * @a: first arguement
+ * @b: second arguement
+ * Return: sum of arguements
+ */
+
+int add(int, int)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
