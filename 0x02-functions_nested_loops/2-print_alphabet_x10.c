67db32e10f4c19eea5bac0265ebfb9cefb28e6ae
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-20 19:01:04 -0700

    added C program to print lower case alphabets 10 time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,11 @@
 
 void print_alphabet_x10(void)
 {
+	int i;
 	char c = 'a';
 	int count = 10;
 
-	for (int i = 1; i <= count; i++)
+	for (i = 1; i <= count; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
