d373ec8b997a86d6365e3e26585a64530f818791
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-20 20:07:56 -0700

    Program to return the absolute value of an argument

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs -program to check if an argument is lowercase
+ * @c: argument to be checked
+ * Return: the absolute value of the argument
+ */
+
+int _abs(int n)
+{
+	return ((n < 0) ? -n : n);
+}
