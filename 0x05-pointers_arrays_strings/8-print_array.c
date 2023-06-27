7ee737c2fba5e47f798f19772a21d05b55fb0537
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-27 11:03:40 -0700

    Added function to print limited number of elements in an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - function that returns the length of a string
+ *
+ * @a: input array
+ *
+ * @n: number of elements of array to be printed
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf('\n')
+
+}
