0cddb21f67507db560a7a8f3bafb7241db6d3b31
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-27 09:50:20 -0700

    function that prits every other character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: is the character to  check
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+
+/**
+ * puts2 - function that prints every other character of a string
+ *
+ * @str: string character to be printed
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int pos = 0;
+	int str_length = _strlen(str);
+
+	while (start <= str_length)
+	{
+		_putchar(str[pos]
+		pos += 2;
+	}
+
+	_putchar("\n")
+}
