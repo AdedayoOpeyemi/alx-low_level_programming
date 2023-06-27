8849abf799381722e91fa620df8c408a1a6d4d76
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-27 02:56:01 -0700

    Added function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - A function that checks for uppercase character.
+ *
+ * @n: is the character to  check
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
