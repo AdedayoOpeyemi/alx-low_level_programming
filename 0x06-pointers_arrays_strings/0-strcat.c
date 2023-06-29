985c53e4f083d1d315ffa47572fd4e723424d9e0
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-29 14:03:37 -0700

    added function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,9 +19,9 @@ char *_strcat(char *dest, char *src)
 	while (*(src + count2))
 	{
 		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
 		count++;
 	}
+	*(dest + count) = '\0';
+
 	return (dest);
 }
