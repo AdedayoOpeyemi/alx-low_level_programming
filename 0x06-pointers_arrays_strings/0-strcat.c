fe2708a98b45d4ab4cabe4d2c4f802c130cd8d7b
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-29 13:57:34 -0700

    added  function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/**
+ * _strcat - A function that swaps 2 integers.
+ *
+ * @dest: destination string to which string is to be concatenated
+ *
+ * @src: secd string to be concatened into the dest string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (*(src + count2))
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
