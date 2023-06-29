7e9e2d55fb9b2c7a82e635203b0b01b936eac993
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-29 14:42:45 -0700

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+/**
+ * _strncpy - Function to copy a string
+ *
+ * @dest: destination string
+ * @src: source string
+ * @i: length of string to be copied
+ *
+ * Return: point to destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+	int count2 = 0;
+
+	int i;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + count) = *(src + count2);
+	}
+
+	*(dest + count) = '\0';
+
+	return (dest);
+}
