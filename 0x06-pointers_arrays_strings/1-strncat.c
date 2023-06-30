228f0b719f4ce36890b20c5707b14847e52c9aa3
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-29 17:41:05 -0700

    function that concatenates 2 strings together

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+/**
+ * _strncat - Function to that concatenates two strings
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes from source string
+ *
+ * Return: pointer to destination string
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+ {
+	int i = 0;
+
+	int dest_length = 0;
+
+	while (*(dest + dest_length) != '\0')
+	{
+		dest_length++;
+	}
+
+	while (i < n && *src)
+	{
+		dest[dest_length + i] = *src;
+		src++;
+		i++;
+	}
+
+	dest[dest_length + i] = '\0';
+	return (dest);
+
+}
