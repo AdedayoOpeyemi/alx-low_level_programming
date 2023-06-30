4b89c10f2c1e571a40968beb7db1bc8c7ffd1154
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-29 19:20:23 -0700

    function to capitalize sentence

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - function that changes all lowercase to uppercase
+ *
+ * @s: string to modify
+ *
+ * Return: s modified string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i - 1] == " " &&  (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
