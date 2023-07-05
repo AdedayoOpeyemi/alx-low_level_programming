0ddeefbe77c1c716503463633df0332c8a8fdb4b
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-05 16:40:56 -0700

    function to print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ *
+ *@s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
