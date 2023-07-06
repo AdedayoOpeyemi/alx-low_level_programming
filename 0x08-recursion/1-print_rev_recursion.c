78cdfbc0272013b8f8bd18bf61db560be6cd9baf
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-05 17:01:51 -0700

    function to print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _print_rev_recursion - function to print string in reverse
+ *
+ * @s: string to be printed in reverse
+ *
+ * Return: void
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
