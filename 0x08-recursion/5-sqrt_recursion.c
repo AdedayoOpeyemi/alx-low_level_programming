25d7915db6cd2083dae354a780d968426ece2027
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-05 19:07:07 -0700

    function to return square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _sqrt - finds the square root of a value
+ * @n: number we are trying to get square root of
+ * @start: start boundary of where check is started
+ * @end: end boundary of where check ends
+ *
+ * Return: the square root
+ */
+int sqrt_checker(int n, int start, int end)
+{
+	if (start > end)
+	{
+		return (-1);
+	}
+
+	int mid = start + (end - start) / 2;
+
+	if (mid == n / mid && n % mid == 0)
+	{
+		return (mid);
+	}
+	else if (mid < n / mid)
+	{
+		return (sqrt_checker(n, mid + 1, end));
+	}
+	else
+	{
+		return (sqrt_checker(n, start, mid - 1));
+	}
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (sqrt_checker(n, 0, n / 2));
+}
