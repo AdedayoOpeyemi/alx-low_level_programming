429b1ff2d3d507cf40577daeb6780b2c41d1047a
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-13 21:19:54 -0700

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,19 +11,23 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	size_t s1_len;
+	size_t s2_len;
+	char *result;
+
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	size_t s1_len = strlen(s1);
-	size_t s2_len = strlen(s2);
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
 	if (n >= s2_len)
 		n = s2_len;
 
-	char *result = malloc((s1_len + n + 1) * sizeof(char));
+	result = malloc((s1_len + n + 1) * sizeof(char));
 
 	if (result != NULL)
 	{
