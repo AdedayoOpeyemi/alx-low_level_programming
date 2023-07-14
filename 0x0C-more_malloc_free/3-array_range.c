6334664c798f2cf641cb3aee7c6a3b95de072a19
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-14 06:36:25 -0700

    updated

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,14 +8,17 @@
  */
 int *array_range(int min, int max)
 {
+	int *arr;
+	int i;
+
 	if (min > max)
 		return (NULL);
 
-	int *arr = malloc((max - min + 1) * sizeof(int));
+	arr = malloc((max - min + 1) * sizeof(int));
 
 	if (arr != NULL)
 	{
-		for (int i = 0; min <= max; i++, min++)
+		for (i = 0; min <= max; i++, min++)
 		{
 			arr[i] = min;
 		}
