fb6e1167e9577aef6b35300ed7cf2b3a747c175e
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-14 19:51:33 -0700

    function to multiply 2 numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,8 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
+int multiply(char *num1, char *num2);
 /**
  * main - multiplies two positive numbers
  * @argc: n arguments
@@ -8,22 +11,55 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	char *num1, *num2;
+	int result;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
 	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+
+	num1 = argv[1];
+	*num2 = argv[2];
+
+	for (int i = 0; num1[i] != '\0'; i++)
+	{
+		if (!isdigit(num1[i]))
+		{
+			printf("Error\n");
+			return (98);
+		}
+	}
+
+	for (int i = 0; num2[i] != '\0'; i++)
+	{
+		if (!isdigit(num2[i]))
+		{
+			printf("Error\n");
+			return (98);
+		}
+	}
+
+	result = multiply(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
+
+/**
+ * multiply - function to multiply
+ * @num1: first number for multiplication
+ * @num2: second number for multiplication
+ * Return: result of multiplication
+ */
+
+int multiply(char *num1, char *num2)
+{
+	int n1, n2, result;
+
+	n1 = atoi(num1);
+	n2 = atoi(num2);
+	result = n1 * n2;
+	return (result);
 }
