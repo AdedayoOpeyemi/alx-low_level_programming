5c37039bb1de39f932881f8f302738ae8a6b38ab
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-17 17:32:51 -0700

    function that prints file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: 0
+ */
 int main(void) {
     printf("Compiled from file: %s\n", __FILE__);
-    return 0;
+    return (0);
 }
