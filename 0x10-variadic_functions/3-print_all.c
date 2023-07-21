5a212d45b7d92005c694db283617de9eb23d3d13
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-20 21:33:52 -0700

    updated

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 
 /**
  * print_all - prints anything.
@@ -8,10 +9,9 @@
  * Return: no return.
  */
 
-void print_all(const char * const format, ...) 
+void print_all(const char * const format, ...)
 {
-	va_list args;
-	va_start(args, format);
+	va_list args;	
 
 	char ch;
 	int num;
@@ -19,34 +19,38 @@ void print_all(const char * const format, ...)
 	char *str;
 	unsigned int i = 0;
 
-	while ((ch = format[i++]) != '\0') {
-		switch (ch) {
-			case 'c':
-				putchar(va_arg(args, int));
-				break;
-			case 'i':
-				num = va_arg(args, int);
-				printf("%d", num);
-				break;
-			case 'f':
-				dbl = va_arg(args, double);
-				printf("%f", dbl);
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str != NULL)
-					printf("%s", str);
-				else
-					printf("(nil)");
-				break;
-			default:
-				break;
-		}
+	va_start(args, format);
 
-		if (format[i] != '\0') {
-			putchar(',');
-			putchar(' ');
-		}
+	while ((ch = format[i++]) != '\0')
+	{
+		switch (ch)
+		{
+		case 'c':
+			_putchar(va_arg(args, int));
+			break;
+		case 'i':
+			num = va_arg(args, int);
+			printf("%d", num);
+			break;
+		case 'f':
+			dbl = va_arg(args, double);
+			printf("%f", dbl);
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str != NULL)
+				printf("%s", str);
+			else
+				printf("(nil)");
+			break;
+		default:
+			break;
+	}
+
+	if (format[i] != '\0') {
+		_putchar(',');
+		_putchar(' ');
+	}
 	}
 
 	va_end(args);
