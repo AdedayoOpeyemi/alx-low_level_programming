d634ebfd02ebda041a0b10b08828df1f879104fd
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-27 21:07:53 -0700

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,10 +9,12 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+
 	if (str == NULL)
 		return (NULL);
 
-	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	new_node = (list_t *)malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
@@ -20,9 +22,10 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 
 	if (new_node->str == NULL)
+	{
 		free(new_node);
 		return (NULL);
-
+	}
 	new_node->len = strlen(str);
 
 	new_node->next = *head;
