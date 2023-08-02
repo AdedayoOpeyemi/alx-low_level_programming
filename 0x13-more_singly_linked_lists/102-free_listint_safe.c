700f9739a472d82e4d924fe18b679cf1377e97e3
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-08-01 18:30:38 -0700

    function to free list safely

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function to safely free list
+ * @h: pointer to pointer of head of list
+ * Return: count of nodes removed
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *current = *h;
+	listint_t *next_node = NULL;
+
+	while (current != NULL)
+	{
+		count++;
+		next_node = current->next;
+		free(current);
+		current = next_node;
+
+		if (current == *h)
+		{
+			*h = NULL;
+			return (count);
+		}
+	}
+
+	*h = NULL;
+	return (count);
+}
